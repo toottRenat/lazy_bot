fe50c3ef143192c5063d29f0496b53d3a23a199c
toottRenat <basonchik@yandex.ru>
2017-06-08 22:41:28 +0300

    Rewrote class-helper lib and made some cosmetic stuff

--- demon_settings.py
+++ demon_settings.py
@@ -1,6 +1,6 @@
 import tkinter.messagebox
-import demon
-from lib.lazy_tkinter import MyEntry, MyButton, MyLabel
+from lazy_bot import demon
+from lazy_bot.lib.lazy_tkinter import MyEntry, MyButton, MyLabel
 import PIL.Image
 import PIL.ImageTk
 from functools import partial
@@ -11,10 +11,13 @@ PREFIX = ''
 
 
 class DemonConfig:
+    start_conf_file = ''.join([PREFIX, 'var/conf/start_config.txt'])
+    skype_contacts_file = ''.join([PREFIX, 'var/skype/contacts.txt'])
+    skype_conf_file = ''.join([PREFIX, 'var/conf/skype_config.txt'])
+
     def __init__(self, root):
         self.root = root
         self.root["bg"] = "azure"
-        #self.root.protocol("WM_DELETE_WINDOW", self.root.withdraw())
         self.__show_menu()
 
     def __show_menu(self):
@@ -36,26 +39,28 @@ class DemonConfig:
 
     def add_skype(self):
         self.__die_root()
-        self.id_label = MyLabel(self.root, 0, 0, "Никнейм или номер телефона контакта:", my_color='azure')
-        self.id_entry = MyEntry(self.root, 0, 1, my_color='lemon chiffon')
+        entries = []
+        id_label = MyLabel(self.root, 0, 0, "Никнейм или номер телефона контакта:", my_color='azure')
+        entries.append(MyEntry(self.root, 0, 1, my_color='lemon chiffon'))
 
-        self.name_label = MyLabel(self.root, 1, 0, "Слово, соответствующее вызову данного контакта:", my_color='azure')
-        self.name_entry = MyEntry(self.root, 1, 1, my_color='lemon chiffon')
+        name_label = MyLabel(self.root, 1, 0, "Слово, соответствующее вызову данного контакта:", my_color='azure')
+        entries.append(MyEntry(self.root, 1, 1, my_color='lemon chiffon'))
 
-        self.accept_button = MyButton(self.root, 2, 1, my_text='Запомнить',
-                                      cur_func=self.add_contact,
-                                      my_color='lemon chiffon')  # скорее всего можно найти и получше цвет для кнопок
+        accept_button = MyButton(self.root, 2, 1, my_text='Запомнить',
+                                 cur_func=partial(self.add_content, entries, self.skype_contacts_file, ' : '),
+                                 my_color='lemon chiffon')  # скорее всего можно найти и получше цвет для кнопок
 
-        self.voice_button = MyButton(self.root,  2, 0, my_text='Использовать голос',
-                                     cur_func=self.get_from_micro, my_width=20,
-                                     my_color='lemon chiffon')
+        voice_button = MyButton(self.root,  2, 0, my_text='Использовать голос',
+                                cur_func=self.get_from_micro, my_width=20,
+                                my_color='lemon chiffon')
 
-    def add_contact(self, _):  # возможно, стоит также сделать возможность удалять и просматривать эти строки
-        if self.name_entry.get() != '' and self.id_entry.get() != '':
-            with open(''.join([PREFIX, 'var/skype/contacts.txt']), 'a') as f:
-                f.write(' : '.join([self.name_entry.get(), self.id_entry.get()]))
+    @staticmethod
+    def add_content(entries, file, joining, _):
+        if '' not in [i.get() for i in entries]:
+            with open(file, 'a') as f:
+                f.write(joining.join([i.get() for i in entries]))
                 f.write('\n')
-                tkinter.messagebox.showinfo('Well done', "Контакт успешно добавлен")
+                tkinter.messagebox.showinfo('Well done', "Данные успешно добавлены")
         else:
             tkinter.messagebox.showerror('Error', "Необходимо заполнить все поля!")
 
@@ -68,40 +73,48 @@ class DemonConfig:
                 break
 
     def change_skype(self):  # картинка не показывается wtf
-        st_file = ''.join([PREFIX, 'var/skype/contacts.txt'])
         self.__die_root()
         src_image = PIL.Image.open(''.join([PREFIX, 'share/button_images/Cross_30x25.jpg']))  # wtf
         img = PIL.ImageTk.PhotoImage(src_image)
         i = 0
-        with open(st_file, 'r') as f:
+        with open(self.skype_contacts_file, 'r') as f:
             for line in f:
                 label = MyLabel(self.root, i, 0, line,
                                 my_color='azure')
                 button = Button(self.root, width=30,
                                 height=25, state='normal')
                 button.grid(row=i, column=1)
-                button.bind("<Button-1>", partial(self.__delete_line, i, st_file, self.change_skype))
+                button.bind("<Button-1>", partial(self.__delete_line, i, self.skype_contacts_file, self.change_skype))
                 button["image"] = img
                 button["compound"] = CENTER
                 i += 1
 
     def start_config(self):
-        st_file = ''.join([PREFIX, 'var/conf/start_config.txt'])
         self.__die_root()
         src_image = PIL.Image.open(''.join([PREFIX, 'share/button_images/Cross_30x25.jpg']))  # мб разные надо
         img = PIL.ImageTk.PhotoImage(src_image)
         i = 0
-        with open(st_file, 'r') as f:
+        with open(self.start_conf_file, 'r') as f:
             for line in f:
-                label = MyLabel(self.root, i, 0, line,
+                label = MyLabel(self.root, i, 3, line,
                                 my_color='azure')
                 button = Button(self.root, width=30,
                                 height=25, state='normal')
-                button.grid(row=i, column=1)
-                button.bind("<Button-1>", partial(self.__delete_line, i, st_file, self.start_config))
+                button.grid(row=i, column=4)
+                button.bind("<Button-1>", partial(self.__delete_line, i, self.start_conf_file, self.start_config))
                 button["image"] = img
                 button["compound"] = CENTER
                 i += 1
+
+        entry = MyEntry(self.root, 0, 0, my_color='lemon chiffon')
+
+        accept_button = MyButton(self.root, 2, 1, my_text='Запомнить',
+                                 cur_func=partial(self.add_content, [entry], self.start_conf_file, ''),
+                                 my_color='lemon chiffon')  # скорее всего можно найти и получше цвет для кнопок
+
+        voice_button = MyButton(self.root,  2, 0, my_text='Использовать голос',
+                                cur_func=self.get_from_micro, my_width=20,
+                                my_color='lemon chiffon')
 
     @staticmethod
     def __delete_line(i, file, caller, _):
@@ -129,5 +142,5 @@ def pseudo_main():
     root.mainloop()
 
 if __name__ == '__main__':
-    PREFIX = '../../'
+    #PREFIX = '../../'
     pseudo_main()
